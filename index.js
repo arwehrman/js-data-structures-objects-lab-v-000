edbd34d400552e90d5fb175aab741b43730806ca
Amanda Wehrman <arwehrman@gmail.com>
2018-01-11 18:20:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,3 +4,7 @@ function updateDriverWithKeyAndValue(driver, key, value) {
   new_driver[key] = value;
   return new_driver
 };
+
+function destructivelyUpdateDriverWithKeyAndValue(driver, key, value) {
+
+}
